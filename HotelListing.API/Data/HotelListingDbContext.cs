e854280a7d559bb1f27a6fbaec6845eb05477595
Mandar Hulaji <mi44120@gmail.com>
2022-08-05 22:12:19 +0530

    Added Entity Framework and Created Database

---
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "India",
+                    ShortName = "IND"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "United States of America",
+                    ShortName = "USA"
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "Australia",
+                    ShortName = "AUS"
+                }
+                );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Sheraton Grand",
+                    Address = "Pune",
+                    CountryId = 1,
+                    Rating = 4.7
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "BloomingDel",
+                    Address = "New Mexico",
+                    CountryId = 2,
+                    Rating = 4.5
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Relax",
+                    Address = "Kankavli",
+                    CountryId = 1,
+                    Rating = 4.0
+                },
+                new Hotel
+                {
+                    Id = 4,
+                    Name = "Lotus",
+                    Address = "Brisbane",
+                    CountryId = 3,
+                    Rating = 4.8
+                },
+                new Hotel
+                {
+                    Id = 5,
+                    Name = "eden Garden",
+                    Address = "Detroit",
+                    CountryId = 2,
+                    Rating = 4.2
+                }
+                );
+        }
+    }
+}
---
+++ HotelListing.API/Migrations/20220805163604_SeededCountriesAndHotels.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelListing.API.Migrations
+{
+    public partial class SeededCountriesAndHotels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[] { 1, "India", "IND" });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[] { 2, "United States of America", "USA" });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[] { 3, "Australia", "AUS" });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[,]
+                {
+                    { 1, "Pune", 1, "Sheraton Grand", 4.7000000000000002 },
+                    { 2, "New Mexico", 2, "BloomingDel", 4.5 },
+                    { 3, "Kankavli", 1, "Relax", 4.0 },
+                    { 4, "Brisbane", 3, "Lotus", 4.7999999999999998 },
+                    { 5, "Detroit", 2, "eden Garden", 4.2000000000000002 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
